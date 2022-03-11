df662141acd2060258575e654cb591f15a712ce4
Noble <nojess143@gmail.com>
2022-03-10 16:57:44 -0800

    A C program that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/** Creating a program to print on C
+ *main - starts the program
+ *return : 0 if success
+ */
+int main(void)
+{
+	puts("\"Programming is like a building in a multilingual puzzle");
+		return (0);
+}
