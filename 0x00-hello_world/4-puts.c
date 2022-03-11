08f2f5cd5532550bc79d79424d2389ed3af856e7
Noble <nojess143@gmail.com>
2022-03-10 17:23:07 -0800

    print C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 /**
- * Creating a program to print on C
- * 
  * main - prints the phrase "Programming is like building a multilingual puzzle
  *
- * return : 0 if success
- *
- *
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
