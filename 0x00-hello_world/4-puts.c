dca8e679fd4b7ff1a5c4316527201f81e73b1ca8
Noble <nojess143@gmail.com>
2022-03-10 17:10:24 -0800

    Print a C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-/** Creating a program to print on C
+/**
+ * Creating a program to print on C
  * 
  * main - Entry point  starts the program
- *
  *
  * return : 0 if success
  *
  *
  */
-int main(void)
+int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
