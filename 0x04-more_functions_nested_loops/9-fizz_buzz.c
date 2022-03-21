323dc954750b1c94234b81116c0c2ec532afb845
Noble <nojess143@gmail.com>
2022-03-21 05:46:27 -0700

    Print 1 -100 and fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,17 +11,17 @@ int main(void)
 
 	n = 1;
 
-	while (n < 100)
+	while (n <= 100)
 	{
-		if ((n % 3 == 0) && (n % 5 == 0))
+		if (n % 3 == 0 && n % 5 == 0)
 		{
 			printf("FizzBuzz");
 		}
-		else if (n % 3 == 0)
+		else if (n % 3 == 0 && n % 5 != 0)
 		{
 			printf("Fizz");
 		}
-		else if (n % 5 == 0)
+		else if (n % 5 == 0 && n % 3 != 0)
 		{
 			printf("Buzz");
 		}
