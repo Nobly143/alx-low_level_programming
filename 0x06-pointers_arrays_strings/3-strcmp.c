3ab71c09e1238cf3e0b984d8278b6a897185e77e
Noble <nojess143@gmail.com>
2022-03-24 08:37:41 -0700

    compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares 2 string
+ * @s1: string 1 to be compared
+ * @s2: string 2 to be compared
+ * Return: integer from comparison
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, j, comp;
+
+	i = 0;
+
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+
+	if (i < j)
+		comp = -15;
+	else if (i == j)
+		comp = 0;
+	else if (i > j)
+		comp = 15;
+	return (comp);
+}
