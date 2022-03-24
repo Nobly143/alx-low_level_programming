71825351ee951bf2c5756a518a258be01ea94625
Noble <nojess143@gmail.com>
2022-03-24 07:53:24 -0700

    Copy strings with bytes

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to copy
+ * Return: pointer to the resulting string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len;
+
+	while (src[len] != '\0' && len < n)
+	{
+		dest[len] = src[len];
+		len++;
+	}
+	dest[len] = '\0';
+
+	return (dest);
+}
