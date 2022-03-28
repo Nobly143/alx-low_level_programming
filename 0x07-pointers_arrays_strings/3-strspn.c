dabdb3c8dc8a97d50eafa52c904eeec9c5d9c690
Noble <nojess143@gmail.com>
2022-03-28 12:39:19 -0700

    count number of characters in common in two strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,19 +9,26 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j, a;
+	unsigned int i, j, a, b;
 
 	a = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		b = 0;
+
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				a++;
+				b = 1;
 			}
 		}
+		if (b == 0)
+		{
+			return (a);
+		}
 	}
-	return (a);
+	return (0);
 }
