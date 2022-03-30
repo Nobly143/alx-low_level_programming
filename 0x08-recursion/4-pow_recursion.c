799febe7ae7fa56224f841f48d58f2fc73fab913
Noble <nojess143@gmail.com>
2022-03-30 14:56:22 -0700

    Do the power of a number to a number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - raise a number
+ * @x: number to be raised
+ * @y: power x is raised to
+ * Return: x raised to the power y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(int x, int y - 1));
+}
