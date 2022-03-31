140814e7a0c6f1044bf5257fd45df6c415325c49
Noble <nojess143@gmail.com>
2022-03-30 20:22:50 -0700

    wildcard

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -33,7 +33,7 @@ int wildcmp(char *s1, char *s2)
 		if (*s2 == '\0')
 			return (1);
 		if (*s1 == *s2)
-			sum += wildcmp(s1 + 1, s2 + 2);
+			sum += wildcmp(s1 + 1, s2 + 1);
 		sum += snatch(s1 + 1, s2);
 		return (!!sum);
 	}
