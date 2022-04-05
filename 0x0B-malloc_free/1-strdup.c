44ca977a4a4eb17d9880da88cee2d19a9a32e5f2
Noble <nojess143@gmail.com>
2022-04-05 08:08:43 -0700

    copy a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_strdup - copy and duplicate a string
+ * @str: string to be copied
+ * Return: pointer to the array (Success), NULL (Error)
+ */
+char *_strdup(char *str)
+{
+	char *a;
+	int len, i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	a = (char *) malloc(len * sizeof(char));
+
+	if (a == NULL)
+		return (NULL);
+
+	i = 0;
+
+	while (i < len)
+	{
+		a[i] = str[i];
+		i++;
+	}
+
+	return (a);
+}
