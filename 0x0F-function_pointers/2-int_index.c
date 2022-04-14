c8ac6971167b28eec8a4f7e9823488c326c022b7
Noble <nojess143@gmail.com>
2022-04-13 17:54:30 -0700

    compare a function with function pointer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - executes a function given as a parameter
+ * that compares an integer
+ * @array: array to iterate over
+ * @size: size of the array
+ * @cmp: pointer to function used
+ * Return: The array that matches or -1 if it fails
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (!array || !cmp || size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		cmp(array[i]);
+		if (cmp != 0)
+			return (array[i]);
+	}
+	return (-1)
+}
