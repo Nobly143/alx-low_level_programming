625f1bca7b4cbc6e2f97ccf890213d28ebed7582
Noble <nojess143@gmail.com>
2022-04-13 21:09:10 -0700

    update task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,9 +8,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int arg1, arg2, result;
-	char o;
-	int (*func)(int, int);
+	int arg1, arg2;
+	int (*operation)(int, int);
 
 	if (argc != 4)
 	{
@@ -18,28 +17,23 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
-
-	func = get_op_func(argv[2]);
-
-	if (!func)
+	if (argv[2][1])
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	o = *argv[2];
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
 
-	if ((o == '/' || o == '%') && arg2 == 0)
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
 	{
 		printf("Error\n");
-		exit(100);
+		exit(99);
 	}
 
-	result = func(arg1, arg2);
-
-	printf("%d\n", result);
-
+	printf("%d\n", operation(arg1, arg2));
 	return (0);
 }
