5f5c66586418c4d2eba97b90b5b2375e9229c245
Noble <nojess143@gmail.com>
2022-04-13 18:15:28 -0700

    compare a function with function pointer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,14 +12,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (!array || size <= 0 || !cmp)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (array && cmp)
 	{
-		cmp(array[i]);
-		if (cmp != 0)
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			cmp(array[i]);
+			if (cmp != 0)
+				return (i);
+		}
 	}
 	return (-1);
 }
