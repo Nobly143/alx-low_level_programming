8b4da503e43d51f43da2f9ebe80d160311d506ce
Noble <nojess143@gmail.com>
2022-04-15 11:14:05 -0700

    Print strings

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,7 +6,7 @@
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *seperator, const unsigned int n, ...);
-void print_string(const char *seperator, const unsigned int n, ...);
+void print_strings(const char *seperator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 #endif
