cb280bd59fe48f4eb62fba3679bcff75909cb4d6
Noble <nojess143@gmail.com>
2022-04-25 15:51:00 -0700

    Retrieve a node at a linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,7 +11,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp = head;
 	unsigned int n = 0;
 
-	while (temp && i < index)
+	while (temp && n < index)
 	{
 		temp = temp->next;
 		n++;
