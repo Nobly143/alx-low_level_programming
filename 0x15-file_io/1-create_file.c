e8d8ba9dc82e0f0ce13b4244a371cfd6c2d2ae19
Noble <nojess143@gmail.com>
2022-05-02 06:35:30 -0700

    Create a new file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file and fills it with text
+ * @filename: name of the file to create
+ * @text_content: text to write in the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, s, t = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC 0600);
+	if (fd < 0)
+		return (-1);
+
+	if (text_context)
+	{
+		while (text_context[s])
+			s++;
+		t = write(fd, text_context, s);
+		if (t != s)
+			return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}
